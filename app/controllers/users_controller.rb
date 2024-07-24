2b358153319166e359372b52dc8bcf68e5fefe76
feruncie <fruncie@chicagobooth.edu>
2024-07-24 22:20:35 +0000

    show details user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,16 @@ class UsersController < ApplicationController
   
       render({ :template => "user_templates/index" })
   end 
+
+  def show 
+
+    url_username = params.fetch("path_username")
+
+    matching_usernames = User.where({ :username => url_username })
+
+    @the_user = matching_usernames.at(0)
+
+    render({ :template => "user_templates/show"})
+    #Parameters "path_username => anisa"
+  end 
 end 
